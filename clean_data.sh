f377432138a0f57e6fb98ce493c0d0ff829046b6
Dannver Wu <dannver@berkeley.edu>
2016-04-22 18:22:52 -0700

    fixed bash script

--- clean_data.sh
+++ clean_data.sh
@@ -3,5 +3,6 @@ args="$@"
 
 for file in $args; do
 	lines=($(wc -l $file))
-	tail -$lines $file | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,19 -d ',' > $file
+	new="$file.clean"
+	tail -$lines $file | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,19 -d ',' > $new
 done
