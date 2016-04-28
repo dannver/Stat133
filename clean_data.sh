bcf729430cadd70a698330bed6b883502ca0a0f8
Dannver Wu <dannver@berkeley.edu>
2016-04-28 16:59:14 -0700

    testing clean_data.sh

--- clean_data.sh
+++ clean_data.sh
@@ -15,15 +15,15 @@ for year in `seq 2009 2014`; do
 			url="$yellowpath$year/$yellow$year-$month$extension"
 		fi
 		echo $url
-		wget $url
-		if [ $month -lt 10 ]; then
-			file="$yellow$year-0$month"
-		else
-			file="$yellow$year-$month"
-		lines=($(wc -l "$file$extension"))
-		lines=$((lines-1))
-		new="$file_clean$extension"
- 		tail -$lines $file | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,18 -d ',' > $new
-		rm $file
+		# wget $url
+		# if [ $month -lt 10 ]; then
+		# 	file="$yellow$year-0$month"
+		# else
+		# 	file="$yellow$year-$month"
+		# lines=($(wc -l "$file$extension"))
+		# lines=$((lines-1))
+		# new="$file_clean$extension"
+ 	# 	tail -$lines $file | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,18 -d ',' > $new
+		# rm $file
 	done
 done
