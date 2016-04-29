9e9d3c3f806a5698a7c64b221bd8c94b78d04594
Dannver Wu <dannver@berkeley.edu>
2016-04-28 22:23:58 -0700

    renamed clean_data.sh to scrape_data.sh

--- taxi_data/scrape_data.sh
+++ taxi_data/scrape_data.sh
@@ -23,7 +23,7 @@ for year in `seq 2009 2014`; do
 		lines=($(wc -l "$file$extension"))
 		lines=$((lines-1))
 		new="$file_clean$extension"
- 		tail -$lines $file | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,18 -d ',' > $new
+ 		tail -$lines "$file$extension" | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,18 -d ',' > $new
 		rm "$file$extension"
 	done
 done
