e25f2def75ec33eacf97ed1d19d8606e61425fe7
Dannver Wu <s133-265@scf.Berkeley.EDU>
2016-04-29 13:11:24 -0700

    added taxi_data

--- taxi_data/scrape_data.sh
+++ taxi_data/scrape_data.sh
@@ -8,22 +8,24 @@ green="green_tripdata_"
 extension=".csv"
 
 for year in `seq 2009 2014`; do
-	for month in `seq 1 12`; do
-		if [ $month -lt 10 ]; then
-			url="$yellowpath$year/$yellow$year-0$month$extension"
-		else
-			url="$yellowpath$year/$yellow$year-$month$extension"
-		fi
-		wget $url
-		if [ $month -lt 10 ]; then
-			file="$yellow$year-0$month"
-		else
-			file="$yellow$year-$month"
-		fi
-		lines=($(wc -l "$file$extension"))
-		lines=$((lines-1))
-		new="$file_clean$extension"
- 		tail -$lines "$file$extension" | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,18 -d ',' > $new
-		rm "$file$extension"
+    for month in `seq 1 12`; do
+	echo "*******Downloading $year-$month*******"
+	if [ $month -lt 10 ]; then
+	    url="$yellowpath$year/$yellow$year-0$month$extension"
+	    else
+	    url="$yellowpath$year/$yellow$year-$month$extension"
+	    fi
+	wget $url
+	if [ $month -lt 10 ]; then
+	    file="$yellow$year-0$month"
+	    else
+	    file="$yellow$year-$month"
+	    fi
+	lines=($(wc -l "$file$extension"))
+	lines=$((lines-1))
+	new=$file"_clean"$extension
+	echo "*******Cleaning $file$extension*******"
+	tail -$lines "$file$extension" | shuf -n 20000 | cut -f 2,3,4,5,6,7,10,11,12,13,18 -d ',' > $new
+	rm "$file$extension"
 	done
 done
