9b9cd5705cc9199b55610be42cffd73de8f3cbe5
Dannver Wu <dannver@berkeley.edu>
2016-04-30 18:22:47 -0700

    added bash script to add column names, trying to fix 2012 data

---
+++ taxi_data/fix_column_names.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# file="yellow_tripdata_2009-01_clean.csv"
+# col_names="p_time,d_time,num_passengers,distance,p_lat,p_long,d_lat,d_long,payment_type,fare,total"
+# echo $col_names | cat - $file > "temp"
+# mv "temp" $file
+
+yellow="yellow_tripdata_"
+green="green_tripdata_"
+extension=".csv"
+col_names="p_time,d_time,num_passengers,distance,p_lat,p_long,d_lat,d_long,payment_type,fare,total"
+
+for year in `seq 2009 2014`; do
+    for month in `seq 1 12`; do
+		if [ $month -lt 10 ]; then
+		    file="$yellow$year-0$month"
+		else
+		    file="$yellow$year-$month"
+		fi
+		file=$file"_clean"$extension
+
+		echo $col_names | cat - $file > "temp"
+		mv "temp" $file
+	done
+done
